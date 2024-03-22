ed5168f39299b041afcdd78963e1e1de088253c3
AdelleAled <adelle.ghys@rubika-edu.com>
2024-03-22 15:42:33 +0100

    on va appeler Seb hein

--- Assets/Art/Art_VFX/SG_shadow/Update/GetLights.hlsl
+++ Assets/Art/Art_VFX/SG_shadow/Update/GetLights.hlsl
@@ -1,4 +1,5 @@
 
+//probleme que j'avais eu: je n'avais pas mis _float apres le nom de la fonction
 void GetLights_float (out float3 mainLightDirection, out float3 mainLightColor, out float3 secondaryLightDirection, out float3 secondaryLightColor) 
 {
     #if defined(SHADERGRAPH_PREVIEW)
@@ -8,14 +9,15 @@ void GetLights_float (out float3 mainLightDirection, out float3 mainLightColor, 
         secondaryLightColor = float3(1, 1, 1);
     #else
     
+        
         float4 lightColor;
         float4 lightPos;
+        
+
+        GetMainLight( mainLightDirection, mainLightColor);
 
         
-        GetMainLight(mainLightDirection, mainLightColor);
-
-        
-        GetSecondaryLight(0, lt, lightColor, lightPos);
+        GetSecondaryLight(0,  lightColor, lightPos);
         secondaryLightDirection = normalize(lightPos.xyz);
         secondaryLightColor = lightColor.rgb;
     #endif
