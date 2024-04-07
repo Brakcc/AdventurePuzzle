bed4e31ff5c14c17afc0d99f3ff087ead59a14e3
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-04-07 18:28:02 +0200

    New Test

---
+++ Assets/Scripts/GameContent/PlayerScripts/PlayerStates/InteractState.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+namespace GameContent.PlayerScripts.PlayerStates
+{
+    public class InteractState : AbstractPlayerState
+    {
+        #region constructor
+
+        public InteractState(GameObject go) : base(go)
+        {
+        }
+
+        #endregion
+        
+        #region methodes
+        
+        public override void OnEnterState(PlayerStateMachine stateMachine)
+        {
+            _stateMachine = stateMachine;
+
+            _checker = _stateMachine.checker;
+
+            _rb.drag = _datasSo.groundingDatasSo.dragSpeed;
+
+            _absorbTimeCounter = _datasSo.interactDatasSo.absorbTime;
+        }
+
+        public override void OnExitState(PlayerStateMachine stateMachine)
+        {
+        }
+
+        public override void OnUpdate()
+        {
+            SetAbsorbTime();
+            GetOtherActionInput();
+            OnAction();
+            
+            SetCoyote();
+            SetJumpBuffer();
+            //OnInputVal();
+        }
+
+        public override void OnFixedUpdate()
+        {
+            //OnJump();
+        }
+
+        #region absorb methodes
+
+        private void SetAbsorbTime()
+        {
+            if (_datasSo.absorbInput.action.IsPressed())
+            {
+                _absorbTimeCounter -= Time.deltaTime;
+                return;
+            }
+            
+            _stateMachine.OnSwitchState(_stateMachine.playerStates[0]);
+        }
+
+        private void GetOtherActionInput()
+        {
+            if (_datasSo.applyInput.action.WasPressedThisFrame())
+                _stateMachine.OnSwitchState("apply");
+        }
+        
+        private void OnAction()
+        {
+            if (_absorbTimeCounter > 0)
+                return;
+
+            if (_checker.InterRef != null)
+                _checker.InterRef.PlayerAction();
+            _stateMachine.OnSwitchState(_stateMachine.playerStates[0]);
+        }
+        
+        #endregion
+        
+        #region jumpSwitchers
+
+        private void OnJump()
+        {
+            if ((!(_coyoteTimeCounter >= 0) || !_datasSo.jumpInput.action.IsPressed()) &&
+                (!(_jumpBufferCounter >= 0) || !IsGrounded))
+                return;
+            
+            _stateMachine.OnSwitchState(_stateMachine.playerStates[1]);
+        }
+        
+        private void SetCoyote()
+        {
+            if (IsGrounded)
+                _coyoteTimeCounter = _datasSo.jumpDatasSo.coyoteTime;
+
+            else
+                _coyoteTimeCounter -= Time.deltaTime;
+
+            _coyoteTimeCounter = Mathf.Clamp(_coyoteTimeCounter, Constants.SecuValuUnderZero, _datasSo.jumpDatasSo.coyoteTime);
+        }
+
+        private void SetJumpBuffer()
+        {
+            if (_datasSo.jumpInput.action.IsPressed())
+                _jumpBufferCounter = _datasSo.jumpDatasSo.jumpBuffer;
+
+            else
+                _jumpBufferCounter -= Time.deltaTime;
+            
+            _jumpBufferCounter = Mathf.Clamp(_jumpBufferCounter, Constants.SecuValuUnderZero, _datasSo.jumpDatasSo.jumpBuffer);
+        }
+
+        #endregion
+        
+        #region moveSwitchers
+
+        private void OnInputVal()
+        {
+            var input = _datasSo.moveInput.action.ReadValue<Vector2>();
+           
+            if (input.magnitude >= Constants.MinMoveInputValue)
+                _stateMachine.OnSwitchState(_stateMachine.playerStates[0]);
+        }
+        
+        #endregion
+        
+        #endregion
+
+        #region fields
+
+        private InterCheckerState _checker;
+        
+        private float _absorbTimeCounter;
+
+        private float _coyoteTimeCounter;
+
+        private float _jumpBufferCounter;
+
+        #endregion
+    }
+}
