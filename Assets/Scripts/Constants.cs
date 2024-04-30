62baafbca233d4b297b04493e809e6a9b8b2f46e
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-05-01 00:50:08 +0200

    petit polish sur le grab pour les lim dist
    penser à corriger les bugs de collisions quand on pull et arrive contre un mur (risque de propulsion de l'actor)
    
    => corriger la ligne de rb.pos en multipliant par un coef de vitesse du player pour eviter l'avance de l'objet / juste if|return de verif avec la vitesse actu du perso selon un seuil

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -7,5 +7,6 @@ public static class Constants
     public const float MinPlayerRotationAngle = 0.05f;
     public const float SecuValuUnderZero = -0.1f;
     public const float JumpTimerAfterInput = 0.15f;
-    public const float RecepMoveSpeedMultiplier = 4f;
+    public const float RecepMoveSpeedMultiplier = 4.1f;
+    public const float GrabGabThreshold = 0.5f;
 }
