b70d892a9035aa2a4b0dd53314942c5742487e05
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-05-01 18:51:14 +0200

    test

--- Assets/Scripts/GameContent/PlayerScripts/PlayerDatas/PlayerDatasSO/MoveDatasSO.cs
+++ Assets/Scripts/GameContent/PlayerScripts/PlayerDatas/PlayerDatasSO/MoveDatasSO.cs
@@ -7,13 +7,13 @@ namespace GameContent.PlayerScripts.PlayerDatas.PlayerDatasSO
     [CreateAssetMenu(fileName = "MoveDatas", menuName = "PlayerDatasSO/MoveDatas")]
     public class MoveDatasSO : AbstractPlayerDatasSO
     {
-        [FieldColorLerp(FieldColor.Orange, FieldColor.Cyan, 1, 30)]
-        [Range(1, 30)]public float moveSpeed;
-
-        [FieldColorLerp(FieldColor.Orange, FieldColor.Cyan, 1, 30)]
-        [Range(1, 30)]public float holdingRecepMoveSpeed;
-        
-        [FieldColorLerp(FieldColor.Orange, FieldColor.Cyan, 1, 15)]
-        [Range(1, 15)] public float rotaSpeedCoef;
+        [FieldColorLerp(FieldColor.Orange, FieldColor.Cyan, 1, 30)]//git
+        [Range(1, 30)]public float moveSpeed;//GIT
+//GIT
+        [FieldColorLerp(FieldColor.Orange, FieldColor.Cyan, 1, 30)]//GIT
+        [Range(1, 30)]public float holdingRecepMoveSpeed;//GIT
+        //GIT
+        [FieldColorLerp(FieldColor.Orange, FieldColor.Cyan, 1, 15)]//GIT
+        [Range(1, 15)] public float rotaSpeedCoef;//GIT
     }
 }
