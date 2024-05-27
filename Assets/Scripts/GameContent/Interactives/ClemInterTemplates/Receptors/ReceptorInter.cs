085188164f8b60e96ff9623ecb5cddbeea8442e7
PandaWithPixels <pandawithpixels@gmail.com>
2024-05-27 19:52:43 +0200

    Suffering

--- Assets/Scripts/GameContent/Interactives/ClemInterTemplates/Receptors/ReceptorInter.cs
+++ Assets/Scripts/GameContent/Interactives/ClemInterTemplates/Receptors/ReceptorInter.cs
@@ -57,6 +57,11 @@ namespace GameContent.Interactives.ClemInterTemplates.Receptors
             protected set => _isMovable = value;
         }
 
+        public void ToDelete()
+        {
+            Debug.Log(_isMovable);
+        }
+
         public bool HasWaveEnergy
         {
             get => _hasWaveEnergy;
@@ -238,7 +243,6 @@ namespace GameContent.Interactives.ClemInterTemplates.Receptors
 
         public override void InterAction()
         {
-            //Debug.Log($"inter action {this}");
             switch (CurrentEnergyType)
             {
                 case EnergyTypes.None:
