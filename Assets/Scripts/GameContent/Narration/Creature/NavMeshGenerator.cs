30d670527b164b852a93769f560b69bf619e28e3
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-05-28 00:38:26 +0200

    des trucs d'ia

---
+++ Assets/Scripts/GameContent/Narration/Creature/NavMeshGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Unity.AI.Navigation;
+using UnityEngine;
+
+namespace GameContent.Narration.Creature
+{
+    public class NavMeshGenerator : MonoBehaviour
+    {
+        #region methodes
+
+        private void Start()
+        {
+            _reMapCounter = 0;
+        }
+
+        private void Update()
+        {
+            if (_reMapCounter <= Constants.ReMapNavMeshDelay)
+            {
+                _reMapCounter += Time.deltaTime;
+                return;
+            }
+            
+            surface.BuildNavMesh();
+            _reMapCounter = 0;
+        }
+
+        #endregion
+
+        #region fields
+
+        [SerializeField] private NavMeshSurface surface;
+        private NavMeshModifier mod;
+        
+        private float _reMapCounter;
+
+        #endregion
+    }
+}
