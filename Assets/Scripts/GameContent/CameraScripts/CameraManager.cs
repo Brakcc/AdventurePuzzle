3ef4034fe71b193dd1a44d9f3cdc0de8733858c7
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-06-05 01:01:33 +0200

    tentative de cam

--- Assets/Scripts/GameContent/CameraScripts/CameraManager.cs
+++ Assets/Scripts/GameContent/CameraScripts/CameraManager.cs
@@ -4,16 +4,42 @@ namespace GameContent.CameraScripts
 {
     public class CameraManager : MonoBehaviour
     {
-        #region methodes
+        #region properties
+
+        public float AngleOverride
+        {
+            set
+            {
+                _isRotating = true;
+                _previousAngle = transform.eulerAngles.y;
+                _angleOverride = value;
+            }
+        }
+
+        #endregion
         
+        #region methodes
+
         private void Update()
         {
             transform.position = Vector3.SmoothDamp(transform.position,
                                                     playerRef.position,
                                                     ref _vel,
                                                     followDamping);
+
+            if (!_isRotating)
+                return;
+
+            if (Mathf.Abs(_previousAngle - _angleOverride) < 2f)
+            {
+                _isRotating = false;
+                return;
+            }
+
+            _previousAngle += Time.deltaTime * Mathf.Sign(_angleOverride);
+            transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * Mathf.Sign(_angleOverride));
         }
-        
+
         #endregion
 
         #region fields
@@ -24,6 +50,12 @@ namespace GameContent.CameraScripts
         [SerializeField] private Transform playerRef;
 
         private Vector3 _vel;
+
+        private bool _isRotating;
+
+        private float _previousAngle;
+        
+        private float _angleOverride;
 
         #endregion
     }
