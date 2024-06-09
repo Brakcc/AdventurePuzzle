cdee95b09c898546c9434a1a6d54522055702fba
PandaWithPixels <pandawithpixels@gmail.com>
2024-06-09 21:42:41 +0200

    ee
    ee

---
+++ Assets/Scripts/GameContent/Interactives/ClemInterTemplates/LineConnector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GameContent.Interactives.ClemInterTemplates.Receptors;
+using UnityEngine;
+
+namespace GameContent.Interactives.ClemInterTemplates
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineConnector : MonoBehaviour
+    {
+        #region methodes
+
+        private void Start()
+        {
+            _line = GetComponent<LineRenderer>();
+        }
+
+        private void Update()
+        {
+            if (recepRef.CurrentEnergyType is not EnergyTypes.Blue)
+            {
+                if (_line.enabled)
+                    _line.enabled = false;
+                return;
+            }
+
+            if (!_line.enabled)
+                _line.enabled = true;
+            
+            _line.SetPositions(new []{startPos.position, targetPos.position});
+        }
+
+        #endregion
+
+        #region fields
+
+        [SerializeField] private ReceptorInter recepRef; 
+            
+        [SerializeField] private Transform startPos;
+
+        [SerializeField] private Transform targetPos;
+        
+        private LineRenderer _line;
+
+        #endregion
+    }
+}
