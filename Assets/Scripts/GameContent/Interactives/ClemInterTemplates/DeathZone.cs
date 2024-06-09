324826855e1222cd30c260454df72a79a19513ee
Meeeal <129063439+Meeeal@users.noreply.github.com>
2024-06-09 23:04:35 +0200

    bvcvvc

---
+++ Assets/Scripts/GameContent/Interactives/ClemInterTemplates/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using GameContent.PlayerScripts;
+using UnityEngine;
+
+namespace GameContent.Interactives.ClemInterTemplates
+{
+    public class DeathZone : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!other.gameObject.CompareTag("Player"))
+                return;
+
+            other.gameObject.GetComponent<PlayerStateMachine>().IsDedge = true;
+        }
+    }
+}
