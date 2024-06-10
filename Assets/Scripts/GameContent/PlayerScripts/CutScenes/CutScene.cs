2749952ab0c931d4948c2684ddd114d78a7b6ff2
PandaWithPixels <pandawithpixels@gmail.com>
2024-06-10 11:28:35 +0200

    eee

---
+++ Assets/Scripts/GameContent/PlayerScripts/CutScenes/CutScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameContent.PlayerScripts.CutScenes
+{
+    public abstract class CutScene : MonoBehaviour
+    {
+        #region constructor
+        
+        protected CutScene(PlayerStateMachine playerMachine)
+        {
+            this.playerMachine = playerMachine;
+        }
+        
+        #endregion
+
+        #region methodes
+
+        public abstract void OnStartCutScene();
+
+        public abstract IEnumerator HandleCutScene();
+
+        public abstract void OnEndCutScene();
+
+        #endregion
+        
+        #region fiedls
+
+        protected PlayerStateMachine playerMachine;
+
+        #endregion
+    }
+}
