4a3e315688c7ac2ddb28334b69ca6b2c5fc90230
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-06-12 06:41:40 +0200

    test

---
+++ Assets/Scripts/DebuggingClem/Chargment.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DebuggingClem
+{
+    public class Chargment : MonoBehaviour
+    {
+        [SerializeField] private float dist;
+        [SerializeField] private Transform pivot;
+        private Transform self;
+        private GameObject player;
+
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            self = transform.GetChild(0);
+        }
+
+        private void Update()
+        {
+            if (Mathf.Abs(Vector3.Distance(player.transform.position, pivot.transform.position)) >= dist)
+            {
+                self.gameObject.SetActive(false);
+            }
+            else
+            {
+                self.gameObject.SetActive(true);
+            }
+        }
+    }
+}
